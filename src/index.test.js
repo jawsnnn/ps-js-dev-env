02bc8f60a7321cc1b41b218d240163ba79675129
jawsnnn <arpanmalviya@gmail.com>
2018-01-01 14:40:14 -0500

    Added test and Appveyor CI config

---
+++ src/index.test.js
@@ -0,0 +1,22 @@
+import {expect} from "chai"; // what is the difference betwee expect and {expect}
+import jsdom from "jsdom";
+import fs from "fs";
+
+// const { JSDOM } = jsdom; // what does this do???
+
+describe('Level 1 test',  function() {
+    it('should pass', function() {
+        expect(true).to.equal(true);
+    });
+});
+
+describe('It should say Hello', function() {
+    it('should say hello', function () {
+        const win = new jsdom.JSDOM(
+            fs.readFileSync('./src/index.html','utf-8') // read contents of the file as a string
+        ).window;
+        const h1 = win.document.getElementsByTagName('h1')[0];
+        expect(h1.innerHTML).to.equal('Hello Kannu');
+        win.close();
+    });
+});
